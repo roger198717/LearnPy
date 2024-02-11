bc6335be88bea3e591f7f0d17457e68e9837346d
sushantrana <roger198717@gmail.com>
2024-02-11 19:45:34 +0530

    Script to pull dynamic temp value from site and write to file every 2 second

--- Lesson3.py
+++ Lesson3.py
@@ -28,7 +28,7 @@ def main():
     time.sleep(2)
     driver.find_element(by="id",value="id_username").send_keys("automated")
     time.sleep(1)
-    driver.find_element(by="id",value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+    driver.find_element(by="id",value="id_password").send_keys("*******" + Keys.RETURN)
     time.sleep(2)
     driver.find_element(by="xpath",value="/html/body/nav/div/a").click()
     i =0
